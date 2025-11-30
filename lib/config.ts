68b5c489657819c56cb86b974836aba40cc1525c
mthivierge392-droid <mthivierge392@gmail.com>
2025-11-30 16:37:01 -0500

    email working

--- lib/config.ts
+++ lib/config.ts
@@ -1,5 +1,5 @@
 export const APP_CONFIG = {
-  supportEmail: 'mthivierge392@gmail.com',
+  supportEmail: 'lynsha@gmail.com',
   urls: {
     stripePayment: 'https://drive.google.com/drive/u/2/folders/1DOVVmEiZDwr3H6Zfnju2nKXm4Ahffgkm'
   }
